f8a5a2612c7e6380a4e9af46557d3788108b52ed
Zijie Wu <noojeff@gmail.com>
2018-11-07 23:08:18 -0500

    Create simplest app

--- example/simple.py
+++ example/simple.py
@@ -6,8 +6,10 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 def is_browser(ua_string):
     return ua_string.split('/')[0].lower() == 'mozilla'
+
 
 @app.route("/")
 def hello():
@@ -20,14 +22,14 @@ def hello():
         response["msg"] = msg_content
         return json.dumps(response)
 
+
 @app.route("/name", methods=["POST"])
 def greeting():
     print(request.data, file=sys.stdout)
     req = json.loads(request.data)
     req["msg"] = "Hi, {}".format(req["name"])
     return json.dumps(req)
-    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(sys.argv[1]))
-
